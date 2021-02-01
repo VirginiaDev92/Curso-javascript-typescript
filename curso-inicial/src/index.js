9e3a36cf21af7130f159418f7b4ad9566fb73939
1cbzin-95 <clodoaldo.brtp4@gmail.com>
2021-02-01 20:44:48 -0300

    add continuação de uso de rotas

--- curso-inicial/src/index.js
+++ curso-inicial/src/index.js
@@ -1,21 +1,28 @@
 const express = require('express');
 
 const app = express(); //crio uma instancia do express
+app.use(express.json());
 
 //criando primeira rota
 app.get('/projects',(request,response)=>{
+    const {title,autor} = request.query;
+    console.log(title,autor);
     return response.json([
         'Projeto 1','Projeto 2','Projeto 3'
     ]);
 });
 
 app.post('/projects',(request,response)=>{
+    const body = request.body;
+    console.log(body);/**pegando tudo que foi enviado pelo cliente */
     return response.json([
         'Projeto 1','Projeto 2','Projeto 3','Projeto 4'
     ]);
 });
 
-app.put('/projects',(request,response)=>{
+app.put('/projects/:id/owner/:owner_id',(request,response)=>{/* nomde e proprietario */
+    const { id,owner_id } = request.params;
+    console.log(id,owner_id);
     return response.json([
         'Projeto 5','Projeto 2','Projeto 3','Projeto 4'
     ]);
