0925081f9e4e69ffbbd4e9db3b244ab819ce146e
“1cbzin-95” <clodoaldo.brtp4@gmail.com>
2021-02-17 23:37:31 -0300

    adicionado transaçao na sequencia de inserçao #1

--- projeto/src/server.ts
+++ projeto/src/server.ts
@@ -9,5 +9,5 @@ app.use(express.json());
 app.use(routes);
 app.use('/uploads',express.static(path.resolve(__dirname,'..','uploads')));
 app.listen(3333,()=>{
-    console.log("Api rodando....");
+    console.log("Api rodando...🤗");
 });
