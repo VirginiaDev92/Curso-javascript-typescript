172354ecad89bbd9ef78d1e51f4ea47b9f2f1122
“1cbzin-95” <clodoaldo.brtp4@gmail.com>
2021-02-19 14:42:16 -0300

    atualizando

---
+++ api-node-coleta/src/database/connection.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+var knex_1 = require("knex");
+var path_1 = require("path");
+var connection = knex_1["default"]({
+    client: 'sqlite3',
+    connection: {
+        filename: path_1["default"].resolve(__dirname, 'database.sqlite')
+    }
+});
+exports["default"] = connection;
